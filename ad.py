b166e20969ec98ca4b127a1e380f03eedb146ae9
VRINDHAB <71087095+VRINDHAB@users.noreply.github.com>
2021-05-16 20:54:05 +0530

    Add files via upload

---
+++ ad.py
@@ -0,0 +1,7 @@
+a=int(input('enter n1: '))
+
+b=int(input('enter n2: '))
+c=int(input('ener n3: '))
+aa=a+b
+d=aa/c
+print(a,"+",b,"/",c,"=",d)
---
+++ am.py
@@ -0,0 +1,7 @@
+a=int(input('enter n1: '))
+b=int(input('enter n2: '))
+c=int(input('enter n3: '))
+aa=a+b
+m=aa*c
+print(a,"+",b,"=",aa)
+print(a,"+",b,"*",c,"=",m)
