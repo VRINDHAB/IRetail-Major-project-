0de47b7f70c0b12167e60137246b34bd69344763
VRINDHAB <71087095+VRINDHAB@users.noreply.github.com>
2021-05-18 14:22:39 +0530

    Add files via upload

---
+++ andoprtr.py
@@ -0,0 +1,9 @@
+a=int(input('enter num1: '))
+b=int(input('enter num2: '))
+c=int(input('enter num3: '))
+if(a>b and a>c):
+	print(a,"is greater")
+elif(b>c):
+	print(b,"is greater")
+else:
+	print(c,"is greatr")
---
+++ as.py
@@ -0,0 +1,7 @@
+a= int(input('enter number1: '))
+b=int( input('enter number2:'))
+sum=a+b
+c=int(input('enter num3: '))
+d=sum-c
+print(a,"+",b,"=",sum)
+print(a,"+",b,"-",c,"=",d)
