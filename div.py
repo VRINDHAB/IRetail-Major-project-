42116ec6a0f3e762890f1dbf406a70cea8a07e71
VRINDHAB <71087095+VRINDHAB@users.noreply.github.com>
2021-05-21 20:56:31 +0530

    Add files via upload

---
+++ div.py
@@ -0,0 +1,4 @@
+a=int(input('enter num1: '))
+b=int(input('enter num2:'))
+div=a/b
+print(a,"/",b,"=",div)
---
+++ divd1.py
@@ -0,0 +1,7 @@
+a=int(input("enter num1: "))
+b=int(input("enter num2: "))
+if(b==0):
+	print("wrong input")
+else:
+	div=a/b
+	print("div",div)
